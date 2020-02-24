7840a959778bf80fa24f4d6216da21b2ee44b497
Ed <lookatemail@gmail.com>
2020-02-24 06:59:01 +0000

    Complete authentication system for users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
 
-  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :set_article, only: [:show, :destroy, :edit, :update]
+  before_action :require_user, except: [:index, :show] 
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -24,7 +26,6 @@ class ArticlesController < ApplicationController
   end
 
   def show
-
   end
 
   def destroy
@@ -54,6 +55,13 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You can only edit or delite your articles"
+      redirect_to root_path
+    end
   end
 
 end
