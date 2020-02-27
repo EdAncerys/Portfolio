4f837738155bc74ede7280dee87a430821c8bbe7
Ed <lookatemail@gmail.com>
2020-02-27 08:54:39 +0000

    Updated all views with categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -35,8 +35,8 @@ class CategoriesController < ApplicationController
   end
   
   def show
-    # @category = Category.find(params[:id])
-    # @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
   
   private
